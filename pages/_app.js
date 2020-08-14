c3bcbd27d1c48985bda3c328748d35b7d4107860
Rodrigo Villalba <rodris.jvo@gmail.com>
2020-08-14 10:56:43 -0400

    Custom heading font and theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
+
+const customTheme = {
+  ...theme,
+  fonts: {
+    ...theme.fonts,
+    heading: "IBM Plex Mono",
+  },
+  colors: {
+    ...theme.colors,
+    screencase: "#b6b6b6",
+  },
+};
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={customTheme}>
       <CSSReset />
       <Component {...pageProps} />
     </ThemeProvider>
